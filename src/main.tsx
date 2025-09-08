263b09753a9a1ba4c6779b69d3b12196f97cb119
aqshola <aqshola.afid@gmail.com>
2025-09-08 20:55:11 +0700

    feat: init multiple component

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import ButtonShowCase from './show/button-showcase'
+import AlertShowCase from './show/alert-showcase'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div>
-      MAGI
+    <div className='flex flex-col gap-3'>
+      <div>
+        MAGI
+      </div>
+      <ButtonShowCase />
+      <AlertShowCase />
     </div>
-    <ButtonShowCase />
+
   </StrictMode>,
 )
---
+++ src/show/alert-showcase.tsx
@@ -0,0 +1,32 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+export default function AlertShowCase() {
+    return <>
+        <div className="flex flex-col gap-4">
+
+            <Alert>
+                <AlertTitle>Success! Your changes have been saved</AlertTitle>
+                <AlertDescription>
+                    This is an alert with icon, title and description.
+                </AlertDescription>
+            </Alert>
+            <Alert>
+                <AlertTitle>
+                    This Alert has a title and an icon. No description.
+                </AlertTitle>
+            </Alert>
+            <Alert variant="destructive">
+                <AlertTitle>Unable to process your payment.</AlertTitle>
+                <AlertDescription>
+                    <p>Please verify your billing information and try again.</p>
+                    <ul className="list-inside list-disc text-sm">
+                        <li>Check your card details</li>
+                        <li>Ensure sufficient funds</li>
+                        <li>Verify billing address</li>
+                    </ul>
+                </AlertDescription>
+            </Alert>
+        </div>
+
+    </>
+}
