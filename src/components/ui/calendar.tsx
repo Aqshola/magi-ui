60a8dbf1b56533cd9b4ba10dd823225d697cd178
aqshola <aqshola.afid@gmail.com>
2025-09-22 21:14:26 +0700

    feat: add custom scroll bar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -64,7 +64,7 @@ function Calendar({
           defaultClassNames.month_caption
         ),
         dropdowns: cn(
-          "w-full  flex items-center text-sm font-medium justify-center h-(--cell-size) gap-1.5",
+          "w-full   flex items-center text-sm font-medium justify-center h-(--cell-size) gap-1.5",
           defaultClassNames.dropdowns
         ),
         dropdown_root: cn(
@@ -72,7 +72,7 @@ function Calendar({
           defaultClassNames.dropdown_root
         ),
         dropdown: cn(
-          "absolute bg-popover text-black inset-0 opacity-0",
+          "absolute custom-dropdown-scroll bg-popover text-black inset-0 opacity-0",
           defaultClassNames.dropdown
         ),
         caption_label: cn(
