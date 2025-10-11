366ab884af7f16a6ab9e9e8c9877993a16387b0b
aqshola <aqshola.afid@gmail.com>
2025-10-12 06:23:46 +0700

    feat: integrate with registy json

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,8 +2,8 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
-import workletURL from '@/utils/paint-worklet.js?url'
-import "@/button-polygon.css"
+import workletURL from '@/lib/paint-worklet.js?url'
+// import "@/button-polygon.css"
 
 const buttonVariants = cva(
   "cursor-pointer uppercase inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-semi-bold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -58,7 +58,7 @@ function Button({
 
   function initButtonPaint() {
     if ("paintWorklet" in CSS) {
-      CSS.paintWorklet.addModule(workletURL)
+      (CSS as any).paintWorklet.addModule(workletURL)
     }
   }
 
